7677c7fa6b0d00822096945b75ffba2d4d32f7c8
Mahabub Alam <mahabubalam3004@gmail.com>
2023-11-12 12:08:03 +0600

    some more code

--- BlogWebsite/src/App.jsx
+++ BlogWebsite/src/App.jsx
@@ -1,11 +1,40 @@
+import {useEffect, useState} from "react";
 import "./App.css";
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice";
+import {Outlet} from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <>
-      <h1>Mohammad Mahabub Alam</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  //!
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (dispatch) {
+          dispatch(logout({userData}));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{Outlet}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+  //here checking if loading is false then show this otherwise do that part
 }
 
 export default App;
